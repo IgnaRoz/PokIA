a6089f694e054ab8b0d2f060c08efdc235a7d037
IGNACIO ROZAS LÓPEZ <Ignacio.Rozas@alu.uclm.es>
2025-03-06 14:01:42 +0100

    metodo comprobar de Proposicion terminado y comprobado

--- Code/brain.py
+++ Code/brain.py
@@ -6,7 +6,7 @@ class SolucionParcial:
     #Cambiar el conjunto y el indice por un simple diccionario????
     
     def __init__(self, variables):
-        self.conjunto = set()
+        self.conjunto = []
         self.indice = {}
         if not isinstance(variables, (list, tuple)):
             raise TypeError("El parámetro 'variables' debe ser una lista o una tupla")
@@ -19,14 +19,17 @@ class SolucionParcial:
         #self.indice = variables
     def add_elemento(self, elemento):
         if isinstance(elemento, dict):
-            lista_vacia = [None]*len(self.indice)
+            nuevo_elemento = [None]*len(self.indice)
             for variable, valor in elemento.items():
-                lista_vacia[self.indice[variable]] = valor
-            elemento = tuple(lista_vacia)
+                #Comprobar que la variable esta en el indice
+                if variable not in self.indice:
+                    raise ValueError("La variable no esta en el indice")
+                nuevo_elemento[self.indice[variable]] = valor
+            elemento = nuevo_elemento
         if not isinstance(elemento, (list, tuple)):
             raise TypeError("El parámetro 'elemento' debe ser una lista o una tupla")
         #Deberia comprobar si elemento es un individuo?
-        self.conjunto.add(elemento)
+        self.conjunto.append(elemento)
     def add_variable(self, variable):
         if not isinstance(variable, Variable):
             raise TypeError("El parámetro 'variable' debe ser del tipo Variable")
@@ -59,98 +62,78 @@ class Proposicion:
                 variables_parcial.append(parametro)
         
         
-
+        # Eliminar variables repetidas mediante set, que no permite elementos repetidos
+        variables_parcial = list(set(variables_parcial))
         solucion_parcial = SolucionParcial(variables_parcial)
-        #conjunto_actual = set()
 
         for elemento_inicial in self.elementos:
-            if len(elemento_inicial) != len(parametros):
+            if len(elemento_inicial) != len(parametros):#deberia bastar con hacerlo una unica vez, pero por si acaso se queda asi
                 raise ValueError("El número de elementos del elemento no coincide con el número de parametros")
 
             elemento_solucion = {}
+            valido = True
             for i, parametro in enumerate(parametros):
+               
                 if isinstance(parametro, Individuo):
                     if elemento_inicial[i] != parametro:
+                        valido = False
                         break
                 elif isinstance(parametro, Variable):
+                    #Se debe comprobar que si la variable ya ha sido añadida al elemento solucion
+                    if parametro in elemento_solucion.keys():
+                        #y si ya ha sido añadida, esta debe de coincidir con el valor del elemento inicial
+                        if elemento_inicial[i] != elemento_solucion[parametro]:
+                            valido = False
+                            break
+                        else:
+                        #y si son iguales, no se debe de volver a añadir,aunque el resultado seria el mismo
+                            continue
+
                     elemento_solucion[parametro] = elemento_inicial[i]
-            solucion_parcial.add_elemento(elemento_solucion.copy())
+            if not valido:
+                continue
+            solucion_parcial.add_elemento(elemento_solucion.copy())#creo que no es necesario hacer una copia
 
         #Combinar la solucion parcial con la solucion inicial
         variables_iniciales = set(solucion_inicial.indice.keys())
         variables_comunes = set(variables_iniciales).intersection(set(variables_parcial))
         variables_nuevas = set(variables_parcial).difference(variables_iniciales)
         solucion_final = SolucionParcial(list(variables_comunes))
+        if len(solucion_inicial.conjunto) == 0:
+            return solucion_parcial
+        
+        for elemento_inicial in solucion_inicial.conjunto:
+            for elemento_parcial in solucion_parcial.conjunto:
+                #Se compara cada elemento de la solucion inicial con cada elemento de la solucion parcial para buscar un nuevo candidato a la solucion final
+                candidato = {}
+                for variable in variables_comunes:
+                    indice_Inicial = solucion_inicial.indice[variable]
+                    indice_Parcial = solucion_parcial.indice[variable]
+                    #Se compara variable a variable
+                    #Y si coiniciden se agrega al nuevo candidato dicha variable
+                    if elemento_inicial[indice_Inicial] == elemento_parcial[indice_Parcial]:
+                        candidato[variable] = elemento_inicial[indice_Inicial]
+                    else:
+                        #Si no coinciden se deja de comparar las siguientes variables, para descartarlo despues
+                        break
+                #Si el numero de variables/individuos en el candidato no coincide con el numero de variables comunes, se descarta
+                if len(candidato.keys())!= len(variables_comunes):
+                    continue
 
-        if len(solucion_inicial.conjunto) > 0:
-            for elemento_inicial in solucion_inicial.conjunto:
-                for elemento_parcial in solucion_parcial.conjunto:
-                    candidato = {}
-                    for variable in variables_comunes:
-                        indice_Inicial = solucion_inicial.indice[variable]
-                        indice_Parcial = solucion_parcial.indice[variable]
-                        
-                        if elemento_inicial[indice_Inicial] == elemento_parcial[indice_Parcial]:
-                            candidato[variable] = elemento_inicial[indice_Inicial]
-                        else:
-                            break
-                    if len(candidato.keys())!= len(variables_comunes):
-                        continue
-
-                    if variables_nuevas:
-                        for variable in variables_nuevas:
-                            indice_Nuevo = solucion_parcial.indice[variable]
-                            candidato[variable] = elemento_parcial[indice_Nuevo]
-                            solucion_final.add_variable(variable)
-                    variables_restantes = variables_iniciales.difference(variables_comunes)
-                    if variables_restantes:
-                        for variable in variables_restantes:
-                            indice_Restante = solucion_inicial.indice[variable]
-                            candidato[variable] = elemento_inicial[indice_Restante]
-                            solucion_final.add_variable(variable)
-                    solucion_final.add_elemento(candidato.copy())
+                if variables_nuevas:#Si hay variables nuevas, se agregan al candidato
+                    for variable in variables_nuevas:
+                        indice_Nuevo = solucion_parcial.indice[variable]
+                        candidato[variable] = elemento_parcial[indice_Nuevo]
+                        solucion_final.add_variable(variable)#Aunque se intenta agregar varias veces la misma variable, no deberia haber problema pues solo se agrega una vez
+                variables_restantes = variables_iniciales.difference(variables_comunes)
+                if variables_restantes:#Si hay variables restantes, se agregan al candidato
+                    for variable in variables_restantes:
+                        indice_Restante = solucion_inicial.indice[variable]
+                        candidato[variable] = elemento_inicial[indice_Restante]
+                        solucion_final.add_variable(variable)
+                solucion_final.add_elemento(candidato.copy())
         return solucion_final
                     
-
-                
-
-
-
-            
-
-# Eliminar metodo viejo
-    def comprobar2(self,solucion_parcial,parametros):
-        if not isinstance(solucion_parcial, SolucionParcial):
-            raise TypeError("El parámetro 'solucion_parcial' debe ser del tipo SolucionParcial")
-        if not isinstance(parametros, list):
-            raise TypeError("El parámetro 'parametros' debe ser una lista")
-        for parametro in parametros:
-            if not isinstance(parametro, (Individuo, Variable)):
-                raise TypeError("Todos los parámetros deben ser del tipo Individuo o Variable")
-        elementos_proposicion = self.elementos.copy()#Puede que haga falta una copia profunda? 
-        for i, parametro in enumerate(self.parametros):
-            if isinstance(parametro, Individuo):
-                #El parametro es un individuo
-                for elemento in elementos_proposicion:
-                    #para cada elemento del conjunto actual de elementos de la proposicion
-                    if elemento[i] != parametro:
-                        #Si el parametro no coincide con el individuo del elemento en la posicion i, este se deve de remover del conjunto actual
-                        elementos_proposicion.remove(elemento)
-                        if elemento in solucion_parcial.conjunto:
-                            #tambien hay que comprobar si el elemento removida ya esta agregado en la solucion parcial y si es asi removerlo.
-                            pass 
-                
-            elif isinstance(parametro, Variable):
-                #El parametro es una variable
-                if parametro in solucion_parcial.indice:
-                    #Es una variable conocida
-                    pass
-                else:
-                    #Es una variable desconocida
-                    pass
-                pass
-            else:
-                raise ValueError("Elemento no reconocido")
 
 
 if __name__ == "__main__":
@@ -162,11 +145,12 @@ if __name__ == "__main__":
     ind_3 = Individuo("ind_3")
     proposicion = Proposicion("proposicion")
     proposicion.elementos.add((ind_1, ind_2))
+    proposicion.elementos.add((ind_1, ind_1))
     proposicion.elementos.add((ind_1, ind_3))
     proposicion.elementos.add((ind_2, ind_3))
     #print(proposicion.elementos)
 
-    solucion_inicial = SolucionParcial([X])
+    solucion_inicial = SolucionParcial([Y])
     solucion_inicial.add_elemento((ind_1,))
     solucion_inicial.add_elemento((ind_3,))
     solucion_final=proposicion.comprobar(solucion_inicial, [X,Y])
